1a909763a4d2b213f4f61585d7d8b8cc51ea6e7c
ludayevip <517950095@qq.com>
2021-05-06 23:33:27 +0800

    修改config diy1sh 文件

--- diy-part1.sh
+++ diy-part1.sh
@@ -40,12 +40,10 @@ git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-p
 git clone https://github.com/sirpdboy/luci-app-autopoweroff package/luci-app-autopoweroff
 # luci-app-godproxy
 git clone https://github.com/godros/luci-app-godproxy package/luci-app-godproxy
-#luci-app-bypass
-git clone https://github.com/tianiue/luci-app-bypass package/luci-app-bypass
 # luci-app-aliddns
 git clone https://github.com/honwen/luci-app-aliddns package/luci-app-aliddns
 # luci-app-vssr
-git clone https://github.com/jerrykuku/lua-maxminddb lede/package/lean/
+git clone https://github.com/jerrykuku/lua-maxminddb lede/package/lean/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr lede/package/lean/luci-app-vssr
 
 
